bf13a8f5bd3e7c6e358cfe943a97d1a59515b3be
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-11-26 12:19:07 -0300

    Add files via upload

---
+++ Media/media.py
@@ -0,0 +1,23 @@
+print("Digite a sua nota do 1 Bimestre:")
+Nota1=input(6)
+
+Nota1_real=float(6)
+
+print("Digite a sua nota 2 Bimestre:")
+Nota2=input(8)
+
+Nota2_real=float(8)
+
+print("Digite a sua nota 3 Bimestre:")
+Nota3=input(8)
+
+Nota3_real=float(8)
+
+print("Digite a sua nota 4 Bimestre:")
+Nota4=input(10)
+
+Nota4_real=float(10)
+
+media=(Nota1_real+Nota2_real+Nota3_real+Nota4_real)/4
+
+print(f"Resultado da Média: {media}")
